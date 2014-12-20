72b86b30b45f83aa395b842ac87c17b0494caf9d
SenhW <senhwang3@gmail.com>
2014-12-20 17:10:23 -0500

    added question to 1.2

--- chapter01/Q1_2/ReverseString.c
+++ chapter01/Q1_2/ReverseString.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/** 
+ * Question 1.2: Implement a function void reverse(char* str) in C or C++
+ * which reverses a null-terminated string.
+ */
+
 void reverse(char* str) {
 	char *start = str; // *start points to first character in str
 	char *end = start + strlen(str) - 1; // *end points to last character in str
